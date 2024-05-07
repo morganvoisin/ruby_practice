ff2ea29cd220ae5d07e89a7f41848436efc2c81f
morgan voisin <morgan.voisin@outlook.com>
2024-05-07 14:38:11 -0600

    Create thith_meanth_war.rb

---
+++ thith_meanth_war.rb
@@ -0,0 +1,18 @@
+=begin
+title: thith_meanth_war.rb
+by: morgan voisin
+date: 05/07/24
+=end
+
+print "Type a thmall paragraph for me pleath. I'll make it thound thuper funny. Make thure to include a lot of th's! "
+user_input = gets.chomp.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else
+  puts "Your paragraph dothn't contain any S's in it my friend. Try running the program again.
+  In the future, I'll be able to loop back to the firtht prompt, but for now re-running ith
+  the best way to thart over. Quack."
+end
+
+puts "I've tranthlated your paragraph to duck-language. Check thith out: #{user_input}, quack. "
