3d62975cd8bdc55628506d747b23ad86ea26ddb3
morgan voisin <morgan.voisin@outlook.com>
2024-05-09 11:42:55 -0600

    Update redacted.rb

--- redacted.rb
+++ redacted.rb
@@ -1,3 +1,9 @@
+# redacted.rb
+# by: morgan voisin
+# date: 05/09/24
+
+# a simple program to take user input, and then redact a specific user-identified word.
+
 puts "Text to search through: "
 text = gets.chomp
 puts "What word do you want redacted? "
